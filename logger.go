b88be6a6782049486b59ec90c8f4899c3bab8be5
uynilo9 <uynilo9@outlook.com>
2024-06-10 00:45:36 +0800

    Fix some typos and code-style typos in the logger.go file

--- logger.go
+++ logger.go
@@ -8,49 +8,49 @@ import (
 )
 
 func Detail(a ...any) (int, error) {
-	a = append([]any{ color.Gray + "DETAIL" + color.Reset + "\n" }, a...)
+	a = append([]any{color.Gray + "DETAIL" + color.Reset + "\n"}, a...)
 	n, err := fmt.Print(a...)
 	return n, err
 }
 
 func Error(a ...any) (int, error) {
-	a = append([]any{ color.Red + "ERROR" + color.Reset + " " }, a...)
+	a = append([]any{color.Red + "ERROR" + color.Reset + " "}, a...)
 	n, err := fmt.Print(a...)
 	return n, err
 }
 
 func Fatal(a ...any) {
-	a = append([]any{ color.Red + "FATAL" + color.Reset + " " }, a...)
+	a = append([]any{color.Red + "FATAL" + color.Reset + " "}, a...)
 	fmt.Print(a...)
 	os.Exit(1)
 }
 
 func Tip(a ...any) (int, error) {
-	a = append([]any{ color.Blue + "TIP" + color.Reset + " " }, a...)
+	a = append([]any{color.Blue + "TIP" + color.Reset + " "}, a...)
 	n, err := fmt.Print(a...)
 	return n, err
 }
 
 func Detailln(a ...any) (int, error) {
-	a = append([]any{ color.Gray + "DETAIL" + color.Reset + "\n" }, a...)
+	a = append([]any{color.Gray + "DETAIL" + color.Reset + "\n"}, a...)
 	n, err := fmt.Println(a...)
 	return n, err
 }
 
 func Errorln(a ...any) (int, error) {
-	a = append([]any{ color.Red + "ERROR" + color.Reset + " " }, a...)
+	a = append([]any{color.Red + "ERROR" + color.Reset}, a...)
 	n, err := fmt.Println(a...)
 	return n, err
 }
 
 func Fatalln(a ...any) {
-	a = append([]any{ color.Red + "FATAL" + color.Reset + " " }, a...)
+	a = append([]any{color.Red + "FATAL" + color.Reset}, a...)
 	fmt.Println(a...)
 	os.Exit(1)
 }
 
 func Tipln(a ...any) (int, error) {
-	a = append([]any{ color.Blue + "TIP" + color.Reset + " " }, a...)
+	a = append([]any{color.Blue + "TIP" + color.Reset}, a...)
 	n, err := fmt.Println(a...)
 	return n, err
 }
